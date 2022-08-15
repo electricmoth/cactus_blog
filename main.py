fdf07d289d33113808628cae4bc3099910f4d896
electricmoth <nope@idonthinkso.tv>
2022-08-14 20:52:08 -0700

    Change to Postgres for Heroku deployment - Add psycopg2-binary==2.9.3, update requirements

--- main.py
+++ main.py
@@ -23,7 +23,9 @@ Bootstrap(app)
 Base = declarative_base()
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# Change to Postgres for Heroku deployment
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
